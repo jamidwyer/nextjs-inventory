a4bec5a8cde4def850deb1e58256b31f34ceab12
Jami <jamidwyer@yahoo.com>
2024-10-10 14:53:50 -0700

    Lint

--- app/components/user-links.tsx
+++ app/components/user-links.tsx
@@ -15,12 +15,15 @@ export default function UserLinks() {
 
   return (
     <div className="flex items-center gap-4">
-      {authenticated ? (<form onSubmit={handleSubmit}>
-        <button className="flex h-[36px] grow items-center justify-end gap-2 rounded-sm bg-coconut p-3 hover:bg-coconut hover:text-licorice md:flex-none md:justify-end md:p-2 md:px-3">
-          <div className="hidden md:block">Sign Out</div>
-        </button>
-      </form>) :
-        (<a href="/login">Sign in</a>)}
+      {authenticated ? (
+        <form onSubmit={handleSubmit}>
+          <button className="flex h-[36px] grow items-center justify-end gap-2 rounded-sm bg-coconut p-3 hover:bg-coconut hover:text-licorice md:flex-none md:justify-end md:p-2 md:px-3">
+            <div className="hidden md:block">Sign Out</div>
+          </button>
+        </form>
+      ) : (
+        <a href="/login">Sign in</a>
+      )}
     </div>
   );
 }
