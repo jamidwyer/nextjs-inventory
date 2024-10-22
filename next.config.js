7abacf9183baf099e28b6e94b3c7c5fc25d55618
Jami <jamidwyer@yahoo.com>
2024-10-22 16:32:47 -0700

    Add redirects to CMS

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/login',
+        destination: 'http://localhost/admin/login/',
+        permanent: true,
+      },
+      {
+        source: '/product/add',
+        destination: 'http://localhost/admin/inventory/product/add/',
+        permanent: true,
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
