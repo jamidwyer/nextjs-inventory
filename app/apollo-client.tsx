3fea11995725c82f20d70a3e5aea4b4aa5d1488c
Jami <jamidwyer@yahoo.com>
2024-10-22 17:02:15 -0700

    Pass token

--- app/apollo-client.tsx
+++ app/apollo-client.tsx
@@ -1,25 +1,31 @@
-import { ApolloClient, HttpLink, InMemoryCache, makeVar } from '@apollo/client';
-import { onError } from '@apollo/client/link/error';
+import {
+  ApolloClient,
+  concat,
+  HttpLink,
+  InMemoryCache,
+  makeVar,
+} from '@apollo/client';
 import { relayStylePagination } from '@apollo/client/utilities';
-
-export const authenticatedVar = makeVar(false);
+import { setContext } from '@apollo/client/link/context';
 
 const httpLink = new HttpLink({
   uri: process.env.API_URL,
-  credentials: 'same-origin',
 });
 
-const logoutLink = onError(({ graphQLErrors }) => {
-  if (
-    graphQLErrors?.length &&
-    (graphQLErrors[0].extensions?.response as any)?.statusCode === 401
-  ) {
-    authenticatedVar(false);
-  }
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('jwt');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
 });
 
 const client = new ApolloClient({
-  link: logoutLink.concat(httpLink),
+  link: concat(authLink, httpLink),
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
